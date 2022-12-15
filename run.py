96895aa9884fabeca629eec7e0852aaec530b2fe
AryanA3735 <98833258+AryanA3735@users.noreply.github.com>
2022-12-15 21:29:08 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,32 +1,4 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Aryan Ali',
-        'title': 'First Blog',
-        'content': 'This is the first blog on my website',
-        'date_posted': 'November 20, 20222, '
-    },
-    {
-        'author': 'Aditya Tiwari',
-        'title': 'Second Blog',
-        'content': 'This is my second blog on this website',
-        'date_posted': 'November 25, 2022'
-    }
-]
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
+from flaskblog import app
 
 if __name__ == '__main__':
     app.run(debug=True)
